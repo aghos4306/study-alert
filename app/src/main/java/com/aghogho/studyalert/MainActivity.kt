492cedbf406eb64030661e1276891679d830192e
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2024-08-05 11:52:05 +0300

    design the top app bar for subject screen

--- app/src/main/java/com/aghogho/studyalert/MainActivity.kt
+++ app/src/main/java/com/aghogho/studyalert/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.aghogho.studyalert.presentation.dashboard.DashboardScreen
+import com.aghogho.studyalert.presentation.subject.SubjectScreen
 import com.aghogho.studyalert.presentation.theme.StudyAlertTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,7 +12,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             StudyAlertTheme {
-                DashboardScreen()
+                //DashboardScreen()
+                SubjectScreen()
             }
         }
     }
